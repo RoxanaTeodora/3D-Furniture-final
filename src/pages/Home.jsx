7a479c83856774440c819421bb8a98a34f9a09fb
Roxana Teodora <catana.roxana97@gmail.com>
2023-11-25 13:21:10 +0200

    first check of style navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+// import React, { useEffect, useState } from "react";
+
+// const Home = () => {
+//   const [products, SetProducts] = useState(null);
+//   useEffect(() => {
+//   const fetchProducts=async ()=>{
+//     const response=await fetch('https://654bc6c15b38a59f28efb3a2.mockapi.io/products');
+
+//   const products=wait response.json();
+//   SetProducts(products);};
+
+// fetchProducts();
+// }, []);
+
+// return products ? ()
+//   return <div>Home</div>;
+// };
+
+// export default Home;
+
+import React from "react";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
