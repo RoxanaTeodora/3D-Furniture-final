bd0141be26ed8cb9fd611167ba9fc703174d3a8b
Roxana Teodora <catana.roxana97@gmail.com>
2023-11-26 22:31:05 +0200

    add products api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,46 @@
-// import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+// import ProductCard from "../components/Products/ProductCard";
+
+const Home = () => {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://652bdb8ed0d1df5273eecf98.mockapi.io/3dproducts"
+      );
+      const products = await response.json();
+      console.log(products);
+      setProducts(products);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return products ? (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          {product.name}
+          <div />
+        </div>
+      ))}
+    </div>
+  ) : (
+    <div>Loading ...</div>
+  );
+  // return <div>Home</div>;
+};
+
+export default Home;
+
+//v1
+// export default Home;
+
+// import React, {useState} from "react";
 
 // const Home = () => {
-//   const [products, SetProducts] = useState(null);
-//   useEffect(() => {
-//   const fetchProducts=async ()=>{
-//     const response=await fetch('https://654bc6c15b38a59f28efb3a2.mockapi.io/products');
-
-//   const products=wait response.json();
-//   SetProducts(products);};
-
-// fetchProducts();
-// }, []);
-
-// return products ? ()
 //   return <div>Home</div>;
 // };
 
 // export default Home;
-
-import React from "react";
-
-const Home = () => {
-  return <div>Home</div>;
-};
-
-export default Home;
